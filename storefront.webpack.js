5679970a53cb5fb01547b69e5d3627b74b645897
Leonardo Matos <leomp120894@gmail.com>
2023-07-04 19:33:39 -0300

    Copying checkout component HTML

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,8 @@ module.exports = () => ({
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
-      './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html')
+      './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
+      './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html')
     }
   }
 })

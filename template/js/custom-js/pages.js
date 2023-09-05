d22ecb2c4566dfb17d90aa300713a98627b4a1cc
matheusgnreis <matheus@e-com.club>
2023-09-05 02:37:43 -0300

    chore: set search with param

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,4 +1,5 @@
 // Add your custom JavaScript for storefront pages here.
+import EcomSearch from '@ecomplus/search-engine'
 import Vue from 'vue'
 import ecomPassport from '@ecomplus/passport-client'
 import AffiliateLink from './components/AffiliateLink.vue'
@@ -27,3 +28,20 @@ $('body').click((e) => {
     $('#instant-search .search__status .close').click()
   }
 })
+
+const newURL = new URLSearchParams(window.location.search)
+console.log(newURL)
+const isShowAll = newURL.get('show_all') || window.sessionStorage.getItem('show_all')
+if (isShowAll === '59$2a82') {
+  window.sessionStorage.setItem('show_all', '59$2a82')
+  EcomSearch.dslMiddlewares.push((dsl) => {
+    console.log('test')
+    dsl.query.bool.filter.forEach(filter => {
+      if (filter && filter.term) {
+        filter.term = {
+          visible: false
+        }
+      }
+    })
+  })
+}

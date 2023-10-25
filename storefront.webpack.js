6352964fcbdaaeecbdf339aa16666c09f0c7f4da
Leonardo Matos <leomp120894@gmail.com>
2023-10-25 18:19:14 -0300

    Copying `APrices` component

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -13,7 +13,9 @@ module.exports = () => ({
       './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/components/PointsApplier.html'),
       './js/PointsApplier.js': path.resolve(__dirname, 'template/js/custom-js/components/PointsApplier.js'),
       './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
-      './html/CartQuickview.html': path.resolve(__dirname, 'template/js/custom-js/components/CartQuickview.html')
+      './html/CartQuickview.html': path.resolve(__dirname, 'template/js/custom-js/components/CartQuickview.html'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
+      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js')
     }
   }
 })

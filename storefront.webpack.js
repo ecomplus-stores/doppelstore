195d5d5955eb7df891e9835c2acb9ed8cd363c1f
Leonardo Matos <leomp120894@gmail.com>
2023-10-25 22:10:25 -0300

    Copying `TheCart` component js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -15,7 +15,8 @@ module.exports = () => ({
       './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
       './html/CartQuickview.html': path.resolve(__dirname, 'template/js/custom-js/components/CartQuickview.html'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
-      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js')
+      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
+      './js/TheCart.js': path.resolve(__dirname, 'template/js/custom-js/components/TheCart.js')
     }
   }
 })

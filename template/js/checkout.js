a5a1520c102934b17dc53cc09746dbafb6eb2af3
matheusgnreis <matheus@e-com.club>
2023-11-23 20:00:24 -0300

    chore: set to cart if brinde is quantity zero

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,26 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+const router1 = window.storefrontApp && window.storefrontApp.router
+setInterval(function () {
+  if (router1) {
+    const emitCheckout1 = (name) => {
+      var itembrinde = ecomCart.data.items.find(item => item.product_id === '655da25d2cd6b65959b3a3b5')
+      if (itembrinde.quantity === 0) {
+       window.location.href = '/app/#/cart'
+       window.alert('Item brinde acabou! Você estará sendo direcionado para o carrinho em breve entrará outro brinde!')
+      }
+    }
+
+    const addRoute1ToData = ({ name }) => {
+      if (name === 'checkout') {
+        emitCheckout1(name)
+      }
+    }
+
+    if (router1.currentRoute) {
+      addRoute1ToData(router1.currentRoute)
+    }
+    router1.afterEach(addRoute1ToData)
+  }
+}, 300)

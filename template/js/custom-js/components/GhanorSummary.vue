54bdbd39163734fcc7763d99ff54b3e5c1d9b73e
matheusgnreis <matheus@e-com.club>
2024-02-16 16:45:51 -0300

    chore: test vue

--- template/js/custom-js/components/GhanorSummary.vue
+++ template/js/custom-js/components/GhanorSummary.vue
@@ -1,26 +1,24 @@
 <template>
     <div id="ghanor-block">
       <template>
-
-        <a-share
-          v-if="currentLocation"
-          class="mb-3"
-          :url="currentLocation"
-          :title="`Compartilhe o projeto`"
-        />
+        <h2>{{ formatMoney(amount) }}</h2>
       </template>
     </div>
   </template>
   
   <script>
 
-  import AShare from '@ecomplus/storefront-components/src/AShare.vue'
+import {
+  i18n,
+  price as getPrice,
+  onPromotion as checkOnPromotion,
+  formatMoney
+} from '@ecomplus/utils'
 
   export default {
     name: 'GhanorSummary',
   
     components: {
-      AShare
     },
   
     data () {
@@ -35,13 +33,10 @@
     },
   
     methods: {
+      formatMoney,
+      
       getPoints () {
-        axios.get('https://us-central1-ecom-ghanor.cloudfunctions.net/app/4566/products', {   
-          headers: {
-            "Content-Type": "application/json",
-            "x-server-token": "423B726488D7972D6A2FABDC6C6E6"
-          }
-        })
+        axios.get('https://us-central1-ecom-ghanor.cloudfunctions.net/app/4566/products')
         .then(({ data }) => {
           if (data && data.data && data.data.length) {
             data.data.forEach(item => {

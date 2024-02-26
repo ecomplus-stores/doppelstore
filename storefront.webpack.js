d55d0936e3467d6815d9e2a278dfeb49d9a55a8b
matheusgnreis <matheus@e-com.club>
2024-02-26 12:25:21 -0300

    chore: update build

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -18,7 +18,7 @@ module.exports = () => ({
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
       './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
       './js/TheCart.js': path.resolve(__dirname, 'template/js/custom-js/components/TheCart.js'),
-      './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/components/DiscountApplier.js'),
+      //'./js/DiscountApplier.js': path.resolve(__dirname, 'template/js/custom-js/components/DiscountApplier.js'),
     }
   }
 })

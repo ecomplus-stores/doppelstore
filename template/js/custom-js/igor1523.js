5a853c327501e7f892922bf4e9e2cb3906301e92
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-03-22 03:36:33 -0300

    Create igor1523.js
    custom js functions made by igor1523

---
+++ template/js/custom-js/igor1523.js
@@ -0,0 +1,12 @@
+function openImage(imageUrl) {
+  var modal = document.getElementById("imageModal");
+  var modalImg = document.getElementById("expandedImage");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = imageUrl;
+  captionText.innerHTML = "Image";
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+}

0fa85c99a5928fc8ff76a765d6d40359aad5c378
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-04-03 09:11:29 -0300

    href

--- template/js/custom-js/igor1523.js
+++ template/js/custom-js/igor1523.js
@@ -1,13 +1,46 @@
 console.log('igor ta on');
-window.openImage = function openImage(imageUrl) {
-  var modal = document.getElementById("imageModal");
-  var modalImg = document.getElementById("expandedImage");
-  var captionText = document.getElementById("caption");
-  modal.style.display = "block";
-  modalImg.src = imageUrl;
-  captionText.innerHTML = "Image";
-  var span = document.getElementsByClassName("close")[0];
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+function updateHrefs() {
+  document.getElementById('sd-660bfba70fc534303de909e6').href = 'https://links.doppelstore.com.br/3xigRYd';
+  document.getElementById('sd-64c590d95e60690370785d73').href = 'https://links.doppelstore.com.br/4aBBtJi';
 }
+
+window.onload = function() {
+  updateHrefs();
+};
+
+// window.openImage = function openImage(imageUrl) {
+//   var modal = document.getElementById("imageModal");
+//   var modalImg = document.getElementById("expandedImage");
+//   var captionText = document.getElementById("caption");
+//   modal.style.display = "block";
+//   modalImg.src = imageUrl;
+//   captionText.innerHTML = "Image";
+//   var span = document.getElementsByClassName("close")[0];
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+// }
+
+            // export default {
+            //   props: {
+            //     giftList: {
+            //       type: Array,
+            //       required: true
+            //     }
+            //   },
+            //   methods: {
+            //     openImage(imageUrl) {
+            //       // Your openImage function logic goes here
+            //       var modal = document.getElementById("imageModal");
+            //       var modalImg = document.getElementById("expandedImage");
+            //       var captionText = document.getElementById("caption");
+            //       modal.style.display = "block";
+            //       modalImg.src = imageUrl;
+            //       captionText.innerHTML = "Image";
+            //       var span = document.getElementsByClassName("close")[0];
+            //       span.onclick = function() {
+            //         modal.style.display = "none";
+            //       };
+            //     }
+            //   }
+            // }

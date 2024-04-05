5a873ce54fc33a7136ad9726072b64891ee587a4
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-04-05 10:28:13 -0300

    chore(components): open component cart item

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,8 @@ module.exports = () => ({
     alias: {
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
+      './html/CartItem.html': path.resolve(__dirname, 'template/js/custom-js/components/CartItem.html'),
+      './js/CartItem.js': path.resolve(__dirname, 'template/js/custom-js/components/CartItem.js'),
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
       './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),

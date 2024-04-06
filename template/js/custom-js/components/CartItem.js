84253e204ee1a18231783e54443b5cfa9a619486
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-04-06 08:57:00 -0300

    Update CartItem.js

--- template/js/custom-js/components/CartItem.js
+++ template/js/custom-js/components/CartItem.js
@@ -16,7 +16,10 @@ import ecomCart from '@ecomplus/shopping-cart'
 import ALink from '@ecomplus/storefront-components/src/ALink.vue'
 import APicture from '@ecomplus/storefront-components/src/APicture.vue'
 import ItemCustomizations from '@ecomplus/storefront-components/src/ItemCustomizations.vue'
-
+const isDoppelGang2 = window.isDoppelgang2 || window.sessionStorage.getItem('isDoppelGang2')
+const isDoppelGang3 = window.isDoppelgang3 || window.sessionStorage.getItem('isDoppelGang3')
+console.log(isDoppelGang2);
+console.log(isDoppelGang3);
 export default {
   name: 'CartItem',
 

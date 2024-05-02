9ae875fd2dc8395c2cb6e863e8e96c3c66a8d0da
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-05-02 05:24:04 -0300

    size label

--- template/js/custom-js/components/EcOrderInfo.js
+++ template/js/custom-js/components/EcOrderInfo.js
@@ -479,13 +479,12 @@ export default {
         this.canModifySubscriptionBonus =  data['can-modify']
         if (this.canModifySubscriptionBonus && this.isBox) {
           window.axios.get(`https://sistema.doppelverso.com.br/ecom/box-tshirt-choice/${this.order.number}`).then(({data}) => {
-             for (const [key, value] of Object.entries(data.options)) {
+            for (const [key, value] of Object.entries(data.options)) {
                 if (value === data.size) {
                   this.sizeLabel = key
                   break
                 }
-              });
-            }
+             }
             console.log(data)
             console.log(sizeLabel)
           })

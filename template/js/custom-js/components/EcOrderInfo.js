7b8e5d997096a61c24a1b5a20cef60aed97d6542
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-05-02 04:51:05 -0300

    size on creation

--- template/js/custom-js/components/EcOrderInfo.js
+++ template/js/custom-js/components/EcOrderInfo.js
@@ -478,6 +478,7 @@ export default {
         this.canModifySubscriptionBonus =  data['can-modify']
         if (this.canModifySubscriptionBonus && this.isBox) {
           window.axios.get(`https://sistema.doppelverso.com.br/ecom/box-tshirt-choice/${this.order.number}`).then(({data}) => {
+            this.size = data.size
             console.log(data)
           })
         }

c000747877ba986cbb122027070bece9c166ccd5
Igor Borges <122545407+igor1523@users.noreply.github.com>
2024-05-02 05:40:23 -0300

    size label context fix

--- template/js/custom-js/components/EcOrderInfo.js
+++ template/js/custom-js/components/EcOrderInfo.js
@@ -477,17 +477,20 @@ export default {
         this.receiveDoppila = data.choice
         this.optionSubscription = data.options
         this.canModifySubscriptionBonus =  data['can-modify']
+        const label = null
         if (this.canModifySubscriptionBonus && this.isBox) {
           window.axios.get(`https://sistema.doppelverso.com.br/ecom/box-tshirt-choice/${this.order.number}`).then(({data}) => {
             for (const [key, value] of Object.entries(data.options)) {
                 if (value === data.size) {
-                  this.sizeLabel = key
+                  sizeLabel = key
                   break
                 }
              }
             console.log(data)
-            console.log(sizeLabel)
+        this.sizeLabel = sizeLabel
+            console.log(this.sizeLabel)
           })
+
         }
       })
       if (!this.skipDataLoad) {
